fe8211218ae03da4e354dfe0d8a2cf7f76391fa2
Anhtt9x <anhtt454598@gmail.com>
2024-06-07 15:49:28 +0700

    version_3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 REPO_NAME = "demo"
 PKG_NAME= "Mongodemo"
 AUTHOR_USER_NAME = "Anhtt9x"
@@ -25,6 +25,7 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires=["pymongo","pymongo[srv]","dnspython","pandas","numpy","ensure","pytest"]
     )
 
 
